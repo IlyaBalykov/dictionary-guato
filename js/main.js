90911c5a8ac1f94193b6f24a3394842bd400a2a4
IlyaBalykov <balykov.ilya.88@gmail.com>
2021-12-25 22:21:23 +0300

    feat: add main section & js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
-const contentSection = document.querySelector('.content');
-const wordValue = document.querySelector('.search-form__input')
+const wordValue = document.querySelector('.search__input')
+const wordExample = document.querySelector('.word-example')
+const translateOutput = document.querySelector('.translate')
+const transcriptOutput = document.querySelector('.transcription')
+const audioExample = document.querySelector('.audio')
+
+function ready() {
+  const createDatalist = (data) => {
+    data.forEach(({por}) => {
+      const wordOption = document.createElement("option");
+      wordOption.value = por;
+      wordExample.append(wordOption);
+    })
+  }
+  console.log("Готов к загрузке слов...")
+  fetch(`https://dictionary-d560e-default-rtdb.firebaseio.com/words.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Получаем слова...")
+        createDatalist(data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+}
+document.addEventListener("DOMContentLoaded", ready);
 
 const search = () => {
   const renderItems = (data) => {
-    console.log()
     console.log(data);
-    data.forEach(({word, image, audio, por, eng}, index) => {
-      if (eng === wordValue.value) {
-        console.log(data[index].eng);
-        renderDiv(data, index)
+    data.forEach(({word, image, audio, por, eng, transcript}, index) => {
+      if (por === wordValue.value) {
+        console.log(data[index].por);
+        renderOutput(data, index)
       }
     })
   }
@@ -23,16 +47,16 @@ const search = () => {
     })
 }
 
-function renderDiv(data, index) {
-  if (contentSection.hasChildNodes()) {
-    contentSection.removeChild(contentSection.childNodes[0])
+function renderOutput(data, index) {
+  if ((translateOutput.value || transcriptOutput.value || audioExample.src) !== "") {
+    translateOutput.value = "";
+    transcriptOutput.value = "";
+    translateOutput.value = `${data[index].word}`;
+    transcriptOutput.value = `${data[index].transcript}`;
+    audioExample.src = `./assets/audio/${translateOutput.value}.wav`
+  } else {
+    translateOutput.value = `${data[index].word}`;
+    transcriptOutput.value = `${data[index].transcript}`;
+    audioExample.src = `./assets/audio/${translateOutput.value}+'wav'`
   }
-  const contentDiv = document.createElement('div')
-  contentDiv.innerHTML = `
-        <div>
-          <span>Translate: </span>
-          <span>${data[index].word}</span>
-        </div> 
-      `
-  contentSection.append(contentDiv);
 }
